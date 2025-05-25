8497770189f1e648308f938a2dcd7dcf9c553108
daniels290813 <daniels@iguazio.com>
2025-05-25 08:47:42 +0300

    update

--- tf-keras/training-and-evaluation.py
+++ tf-keras/training-and-evaluation.py
@@ -178,10 +178,10 @@ def train(
     model = _get_model()
 
     # Apply MLRun's interface for tf.keras:
-    mlrun_tf_keras.apply_mlrun(model=model, model_name="mask_detector", context=context)
+    # mlrun_tf_keras.apply_mlrun(model=model, model_name="mask_detector", context=context)
 
     # Initialize the optimizer:
-    optimizer = keras.optimizers.Adam(lr=lr)
+    optimizer = keras.optimizers.Adam(learning_rate=lr)
 
     # Compile the model:
     model.compile(
